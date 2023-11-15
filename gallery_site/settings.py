57b4467ac705ed5d94ee7350ad8a2dfa2cc8a6ee
emidombek <emilia.maria.dombek@gmail.com>
2023-11-15 11:34:46 +0000

    Add db settings

--- gallery_site/settings.py
+++ gallery_site/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-qc9_8sndkzqr^dutt03+(_fa85f+er2p(%#hh^e8r^&iks^^tj"
-)
+SECRET_KEY = os.enviro.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
