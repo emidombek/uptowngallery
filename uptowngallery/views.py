c0307742b6caea945921a1846106484d7c4121e2
emidombek <emilia.maria.dombek@gmail.com>
2023-11-15 13:36:59 +0000

    Adding landing page view

--- uptowngallery/views.py
+++ uptowngallery/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Artwork, Category
 
-# Create Ir views here.
+
+class LandingPageView(View):
+    def get(self, request):
+        top_categories = Category.objects.all()[:9]
+        recent_artworks = Artwork.objects.filter(
+            approved=True
+        ).order_by("-create_date")[:10]
+        return render(
+            request,
+            "landing_page.html",
+            {
+                "top_categories": top_categories,
+                "recent_artworks": recent_artworks,
+            },
+        )
