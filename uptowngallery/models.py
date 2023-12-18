8509f99def140bf1f95876062dccde8ead523044
emidombek <emilia.maria.dombek@gmail.com>
2023-12-18 23:18:36 +0000

    Removing unneeded extensions + updating templates

--- uptowngallery/models.py
+++ uptowngallery/models.py
@@ -1,13 +1,11 @@
 from datetime import timedelta
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
 from cloudinary.models import CloudinaryField
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.db import IntegrityError
 import logging
 
 logger = logging.getLogger(__name__)
