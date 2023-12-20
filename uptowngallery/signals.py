32aa8e3b74e8e887ae7cea3eec02e35dbd843cb2
emidombek <emilia.maria.dombek@gmail.com>
2023-12-20 09:43:38 +0000

    Styling changes, user signup signal and email/messages

--- uptowngallery/signals.py
+++ uptowngallery/signals.py
@@ -4,36 +4,32 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from allauth.account.signals import user_signed_up
 from django.contrib.auth import get_user_model
-from uptowngallery.models import UserProfile, Artwork, Auction
-from django.utils import timezone
+from uptowngallery.models import UserProfile, Artwork
 import logging
 
 User = get_user_model()
-
 logger = logging.getLogger(__name__)
 
 
-# Signal handler to create or update UserProfile
 @receiver(user_signed_up)
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     try:
         if created:
-            print("User created")
+            logger.info("User created: Creating UserProfile")
             UserProfile.objects.create(user=instance)
         else:
-            print("User updated")
+            logger.info("User updated: Saving UserProfile")
             instance.profile.save()
     except Exception as e:
-        print(f"Error in create_or_update_user_profile: {e}")
+        logger.error(f"Error in create_or_update_user_profile: {e}")
 
 
 @receiver(post_save, sender=Artwork)
 def update_or_create_auction(sender, instance, created, **kwargs):
     logger.info(
-        f"Artwork post_save signal triggered for Artwork: {instance.id}, Created: {created}"
+        f"Artwork post_save signal triggered for Artwork ID: {instance.id}, Created: {created}"
     )
 
-    # Call approve_and_start_auction only when the artwork is approved
     if instance.approval_status == "approved":
         instance.approve_and_start_auction()
