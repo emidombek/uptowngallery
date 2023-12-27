0ce71ee162231cfa66eaa31a3060c5f70274bfa6
emidombek <emilia.maria.dombek@gmail.com>
2023-12-27 10:51:18 +0000

    Adding deletion method to admin view for rejected artworks

--- uptowngallery/admin.py
+++ uptowngallery/admin.py
@@ -1,6 +1,7 @@
 import datetime
 from django.contrib import admin
-from .models import Artwork
+from django.db import transaction
+from .models import Artwork, Auction
 from .forms import ArtworkCreateForm
 
 
@@ -29,6 +30,25 @@ class ArtworkAdmin(admin.ModelAdmin):
     approve_artworks.short_description = "Approve selected artworks"
 
     exclude = ["approved", "auction_start"]
+
+    def delete_queryset(self, request, queryset):
+        """
+        This method is called when admin tries to delete artworks in bulk from the admin interface.
+        """
+        with transaction.atomic():  # Use a transaction to ensure deletion
+            for artwork in queryset:
+                # Handle or delete related auctions first
+                Auction.objects.filter(artwork=artwork).delete()
+                artwork.delete()  # Then delete the artwork itself
+
+    def delete_model(self, request, obj):
+        """
+        This method is called when an admin tries to delete a single artwork instance.
+        """
+        with transaction.atomic():  # Use a transaction to ensure atomic deletion
+            # Handle or delete related auctions first
+            Auction.objects.filter(artwork=obj).delete()
+            obj.delete()  # Then delete the artwork itself
 
 
 admin.site.register(Artwork, ArtworkAdmin)
