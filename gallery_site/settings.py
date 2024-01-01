dc408754155a944637205a0b34e754cd47603be7
emidombek <emilia.maria.dombek@gmail.com>
2024-01-01 15:42:50 +0000

    Add test UserProfileModel

--- gallery_site/settings.py
+++ gallery_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 from allauth.account import app_settings
 
@@ -123,6 +124,9 @@ WSGI_APPLICATION = "gallery_site.wsgi.application"
 DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if "test" in sys.argv:
+    DATABASES["default"]["ENGINE"] = "django.db.backends.sqlite3"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
