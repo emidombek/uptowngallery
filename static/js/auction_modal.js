aa9606c4df43c1f569177afd8dfa47f7952e683a
emidombek <emilia.maria.dombek@gmail.com>
2024-01-09 23:27:18 +0000

    Add comment and clean up code

--- static/js/auction_modal.js
+++ static/js/auction_modal.js
@@ -1,26 +1,18 @@
-console.log('Script loaded');
-
+/* Initializes a modal popup for auction details. When the modal is triggered, it performs an AJAX request 
+to fetch and display details about a specific auction. The auction's ID and artwork's ID are extracted from
+the button that triggered the modal. It then updates the modal content with the fetched data and sets
+the modal title. In case of an AJAX error, it displays an error message within the modal.*/
 $(function () {
-  console.log('Modal script loaded');
-
   $('#auctionModal').on('show.bs.modal', function (event) {
-    console.log('Modal event triggered');
-
     let button = $(event.relatedTarget);
     let artworkId = button.data('artwork-id');
     let auctionId = button.data('auction-id');
-
-    console.log('Artwork ID:', artworkId, 'Auction ID:', auctionId);
-
     $.ajax({
       url: '/auction_detail/' + artworkId + '/' + auctionId + '/',
       type: 'GET',
       success: function (data) {
-        // Update the modal content
         $('#auction-detail-content').html(data);
-
-        // Extract the title from the loaded content and set it as the modal title
-        var title = $('#auction-detail-content').find('h1').first().text();
+        let title = $('#auction-detail-content').find('h1').first().text();
         $('#auctionModalLabel').text(title);
       },
       error: function (jqXHR, textStatus, errorThrown) {
