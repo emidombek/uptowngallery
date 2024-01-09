3f89883ce2af7f2e98b40a184f2f316ad9a91fcf
emidombek <emilia.maria.dombek@gmail.com>
2024-01-09 23:30:53 +0000

    Clean up code and add comments

--- static/js/navbar_status.js
+++ static/js/navbar_status.js
@@ -1,21 +1,27 @@
+/**
+ This script dynamically updates the navigation bar to highlight the active link
+ based on the current page URL. It runs on document ready and performs the following actions:
+
+ Defines the `updateActiveNav` function to manage the active state of navigation links.
+ It first removes the 'active' class from all navigation links.
+
+ Then, it compares each navigation link's href attribute with the current window location.
+ If a match is found, it adds the 'active' class to that link, highlighting it as active.
+ 
+ Invokes the `updateActiveNav` function immediately to set the correct active link when the page loads.
+ Binds the `updateActiveNav` function to the window's 'popstate' event.
+ This ensures that the active link is updated correctly when navigating through browser history (back/forward buttons).
+ */
 $(document).ready(function () {
-  // Function to update the active class on the current page's nav link
   function updateActiveNav() {
-    // Remove 'active' class from all nav links
     $('.nav-item .nav-link').removeClass('active');
-
-    // Get current page URL path
-    var currentPath = window.location.pathname;
-
-    // Add 'active' class to the nav link that corresponds to the current page
+    let currentPath = window.location.pathname;
     $('.nav-item .nav-link').each(function () {
       if ($(this).attr('href') === currentPath) {
         $(this).addClass('active');
       }
     });
   }
-
-  // Run the function on page load
   updateActiveNav();
 
   $(window).on('popstate', function () {
