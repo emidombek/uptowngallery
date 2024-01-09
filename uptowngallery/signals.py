a506eb7fc909be7fa42edd2a752aa130b10258db
emidombek <emilia.maria.dombek@gmail.com>
2024-01-09 22:32:18 +0000

    Reorganized code + added comments

--- uptowngallery/signals.py
+++ uptowngallery/signals.py
@@ -1,13 +1,10 @@
-# signals.py
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from allauth.account.signals import user_signed_up
-from django.contrib.auth import get_user_model
-from uptowngallery.models import UserProfile, Artwork
-from allauth.account.signals import user_signed_up
-from django.dispatch import receiver, Signal
-from django.core.mail import send_mail
 import logging
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.db.models.signals import post_save
+from django.dispatch import receiver, Signal
+from allauth.account.signals import user_signed_up
+from uptowngallery.models import UserProfile, Artwork
 
 User = get_user_model()
 logger = logging.getLogger(__name__)
@@ -20,6 +17,10 @@ profile_updated = Signal()
 
 @receiver(user_signed_up)
 def create_user_profile(sender, request, user, **kwargs):
+    """
+    Signal receiver to create a UserProfile instance when a new user signs up.
+    Logs the creation process and handles any exceptions.
+    """
     try:
         logger.info("User created: Creating UserProfile")
         profile, created = UserProfile.objects.get_or_create(user=user)
@@ -34,6 +35,10 @@ def create_user_profile(sender, request, user, **kwargs):
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
+    """
+    Signal receiver to update the UserProfile instance when the User instance is updated.
+    Saves the profile and logs any exceptions.
+    """
     if not created:
         try:
             logger.info("User updated: Saving UserProfile")
@@ -44,6 +49,10 @@ def update_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=Artwork)
 def update_or_create_auction(sender, instance, created, **kwargs):
+    """
+    Signal receiver to handle the post-save event for an Artwork instance.
+    If the artwork's approval status is 'approved', it triggers the auction approval process.
+    """
     logger.info(
         f"Artwork post_save signal triggered for Artwork ID: {instance.id}, Created: {created}"
     )
@@ -53,12 +62,14 @@ def update_or_create_auction(sender, instance, created, **kwargs):
 
 @receiver(user_signed_up)
 def user_signed_up_(request, user, **kwargs):
-    # Send an email to the new user
+    """
+    Signal receiver to send a welcome email to the user upon successful sign-up.
+    """
     send_mail(
         "Welcome to Uptown Gallery",
         "Thank I for signing up for our site!",
-        "mailto@uptowngallery.com",  # Replace with my sender email
-        [user.email],  # User's email address
+        "mailto@uptowngallery.com",
+        [user.email],
         fail_silently=False,
     )
 
@@ -67,7 +78,10 @@ def user_signed_up_(request, user, **kwargs):
 def send_artwork_approval_notification(
     sender, artwork, request, **kwargs
 ):
-    # Send an email notification
+    """
+    Signal receiver to send an email notification when an artwork is approved.
+    Notifies the artist that their artwork has been approved and the auction has started.
+    """
     send_mail(
         "Your Artwork Has Been Approved!",
         'Your artwork "{}" has been approved and your auction has started.'.format(
@@ -83,36 +97,35 @@ def send_artwork_approval_notification(
 def send_artwork_denial_notification(
     sender, artwork, request, **kwargs
 ):
+    """
+    Signal receiver to send an email notification when an artwork is denied.
+    Notifies the artist that their artwork has been denied and will be deleted.
+    """
     send_mail(
         "Your Artwork Has Been Denied",
         'Unfortunately, your artwork "{}" has been denied and will be deleted.'.format(
             artwork.title
         ),
         "mailto@uptowngallery.com",
-        [artwork.artist.user.email],  # Use the user's email
+        [artwork.artist.user.email],
         fail_silently=False,
     )
 
 
 @receiver(auction_closed)
 def auction_closed_email_notification(sender, auction, **kwargs):
-    # Accessing the artist's user's email based on my models' structure
+    """
+    Signal receiver to send an email notification when an auction is closed.
+    Notifies the artist of the auction's closure.
+    """
     artist_user_email = auction.artwork.artist.user.email
-
-    # Define the email content
     subject = f"Auction for {auction.artwork.title} has ended"
     message = (
         f"Dear {auction.artwork.artist.name},\n\n"
         f"The auction for the artwork '{auction.artwork.title}' has ended."
     )
-    from_email = (
-        "mailto@uptownfgallery.com"  # replace with my actual email
-    )
-
-    # Using the correctly accessed email
+    from_email = "mailto@uptownfgallery.com"
     recipient_list = [artist_user_email]
-
-    # Send the email
     send_mail(
         subject,
         message,
@@ -124,7 +137,10 @@ def auction_closed_email_notification(sender, auction, **kwargs):
 
 @receiver(bid_placed)
 def send_notification_emails(sender, bid, user, **kwargs):
-    # Sending email to the bidder
+    """
+    Signal receiver to send email notifications upon placing a bid.
+    Notifies both the bidder and the artwork's artist about the new bid.
+    """
     send_mail(
         subject="Bid Confirmation",
         message=f"Your bid of {bid.amount} has been placed successfully on {bid.auction.artwork.title}.",
@@ -132,8 +148,6 @@ def send_notification_emails(sender, bid, user, **kwargs):
         recipient_list=[user.email],
         fail_silently=False,
     )
-
-    # Sending email to the artist if the artist is defined and has an email attribute
     artist = bid.auction.artwork.artist
     if artist and hasattr(artist, "email") and artist.email:
         send_mail(
@@ -147,6 +161,10 @@ def send_notification_emails(sender, bid, user, **kwargs):
 
 @receiver(profile_updated)
 def send_profile_update_email(sender, user, field, new_value, **kwargs):
+    """
+    Signal receiver to send email notifications when a user's profile is updated.
+    Handles specific fields like 'shipping_address'.
+    """
     if field == "shipping_address":
         send_mail(
             subject="Shipping Address Updated",
@@ -159,6 +177,10 @@ def send_profile_update_email(sender, user, field, new_value, **kwargs):
 
 @receiver(profile_updated)
 def send_profile_update_email(sender, user, field, new_value, **kwargs):
+    """
+    Signal receiver to send email notifications when a user's profile is updated.
+    Handles specific fields like 'name'.
+    """
     if field == "name":
         send_mail(
             subject="Name Updated",
