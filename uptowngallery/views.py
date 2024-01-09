abac33f29154d40e62eaec67a9119f4c24123215
emidombek <emilia.maria.dombek@gmail.com>
2024-01-09 20:14:44 +0000

    Reorganizing code and adding more comments correcting spelling etc.

--- uptowngallery/views.py
+++ uptowngallery/views.py
@@ -1,29 +1,26 @@
-import logging
-from .forms import ArtworkCreateForm, CustomSignupForm, BidForm
-from .signals import bid_placed, profile_updated
-from django.http import JsonResponse
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.middleware.csrf import get_token
+from django.db import transaction
 from django.db.models import Q, Max, Prefetch
-from django.views import View
-from django.views.generic import TemplateView
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import CreateView
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.utils import timezone
-from django.db import transaction
-from django.shortcuts import get_object_or_404
-from .signals import bid_placed
-from .models import (
-    Artwork,
-    Auction,
-    Bids,
-)
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views import View
+from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.middleware.csrf import get_token
+from .forms import ArtworkCreateForm, CustomSignupForm, BidForm
+from .models import Artwork, Auction, Bids
+from .signals import bid_placed, profile_updated
 
 
 class LandingPageView(View):
+    """
+    Retrieve 10 most recent artowrks to display
+    """
+
     def get(self, request):
         recent_artworks = Artwork.objects.filter(
             approved=True
@@ -38,29 +35,28 @@ class LandingPageView(View):
 
 
 class ArtworkListView(View):
-    def get(self, request):
-        category = request.GET.get(
-            "category"
-        )  # Get the category from the request
+    """
+    Gets the category from the request
+    Filters by category
+    Annotate each artwork with the ID of its most recent active auction
+    Prefetch the related recent active auction for each artwork
+    Implement Pagination,show 10 artworks per page
+    Pass the category to the template
+    """
 
+    def get(self, request):
+        category = request.GET.get("category")
         artworks = Artwork.objects.filter(
             approval_status="approved", auctions__status="active"
         ).distinct()
-
         artworks = artworks.order_by("-create_date")
-
-        # Filter by category if it's provided
         if category:
             artworks = artworks.filter(category=category)
-
-        # Annotate each artwork with the ID of its most recent active auction
         artworks = artworks.annotate(
             recent_auction_id=Max(
                 "auctions__id", filter=Q(auctions__status="active")
             )
         )
-
-        # Prefetch the related recent active auction for each artwork
         artworks = artworks.prefetch_related(
             Prefetch(
                 "auctions",
@@ -69,14 +65,12 @@ class ArtworkListView(View):
                         artwork.recent_auction_id
                         for artwork in artworks
                     ],
-                    status="active",  # Fetch only active auctions
+                    status="active",
                 ),
                 to_attr="recent_auction",
             )
         )
-
-        # Implement Pagination
-        paginator = Paginator(artworks, 10)  # Show 10 artworks per page
+        paginator = Paginator(artworks, 10)
         page_number = request.GET.get("page")
         page_obj = paginator.get_page(page_number)
 
@@ -85,29 +79,37 @@ class ArtworkListView(View):
             "artwork_list.html",
             {
                 "page_obj": page_obj,
-                "category": category,  # Pass the category to the template
+                "category": category,
             },
         )
 
 
 class CreateArtworkView(LoginRequiredMixin, CreateView):
+    """
+    Sets form and template name
+    Redirects to pending artworks page if successful
+    Set initial auction-related fields in the Artwork instance to None
+    Set the 'approval_status' attribute of the form's instance to "pending"
+    Call the parent class's `form_valid` method to continue processing the form submission if form is valid
+    Save the form's instance to the database
+    Prints errors if form is not valid
+    Logging the context data and returning it without modifications
+    Logging the POST data and then calling the parent class's post method to handle the form submission
+    """
+
     template_name = "create_artwork.html"
     form_class = ArtworkCreateForm
     success_url = reverse_lazy("pending_artworks")
 
     def form_valid(self, form):
         form.instance.artist = self.request.user.profile
-        # Set initial auction-related fields in the Artwork instance
-        form.instance.auction_start = (
-            None  # Auction will start upon approval
-        )
-        form.instance.approval_status = "pending"  # Initial status
-        # Save the Artwork instance
+        form.instance.auction_start = None
+        form.instance.approval_status = "pending"
         return super().form_valid(form)
 
     def form_invalid(self, form):
         print("Form is invalid")
-        print(form.errors)  # Print the form errors
+        print(form.errors)
         messages.error(
             self.request,
             "Artwork creation failed. Please check the form.",
@@ -125,66 +127,69 @@ class CreateArtworkView(LoginRequiredMixin, CreateView):
 
 
 class PendingArtworksView(LoginRequiredMixin, View):
+    """
+    Filter artworks to show only pending artworks of the current user
+    Paginate the artworks, show 10 artworks per page
+    """
+
     def get(self, request):
-        # Filter artworks to show only pending artworks of the current user
         artworks = Artwork.objects.filter(
             artist=request.user.profile, approved=False
         )
-
-        # Paginate the artworks
-        paginator = Paginator(artworks, 10)  # Show 10 artworks per page
+        paginator = Paginator(artworks, 10)
         page = request.GET.get("page")
-
         try:
             artworks = paginator.page(page)
         except PageNotAnInteger:
             artworks = paginator.page(1)
         except EmptyPage:
             artworks = paginator.page(paginator.num_pages)
-
         return render(
             request, "pending_artworks.html", {"artworks": artworks}
         )
 
 
 def signup_view(request):
+    """
+    View that sets a custom sign up form, validate and save.
+    """
     if request.method == "POST":
         form = CustomSignupForm(request.POST)
-
         if form.is_valid():
             user = form.save(request)
             return redirect("verification_sent")
     else:
         form = CustomSignupForm()
-
     return render(request, "account/signup.html", {"form": form})
 
 
 class AuctionDetailView(LoginRequiredMixin, View):
+    """
+    Get associated artworks and auctions
+    Calculate current price
+    Create an emplty instance of the bidform
+    Route user based on request type
+    Ensure bid amount is entered without cents
+    """
+
     def get(self, request, artwork_id, auction_id):
         artwork = get_object_or_404(Artwork, pk=artwork_id)
         auction = get_object_or_404(
             Auction, pk=auction_id, artwork=artwork
         )
-
-        # Calculate the current price
         bids = Bids.objects.filter(auction=auction).order_by("-amount")
         current_price = (
             bids.first().amount
             if bids.exists()
             else auction.reserve_price
         )
-
-        # Create an empty instance of the BidForm
         form = BidForm()
-
         context = {
             "auction": auction,
             "artwork": artwork,
             "current_price": current_price,
             "form": form,
         }
-
         if request.headers.get("X-Requested-With") == "XMLHttpRequest":
             return render(
                 request, "auction_detail_partial.html", context
@@ -197,12 +202,9 @@ class AuctionDetailView(LoginRequiredMixin, View):
         auction = get_object_or_404(
             Auction, pk=auction_id, artwork=artwork
         )
-
         form = BidForm(request.POST, auction=auction)
         if form.is_valid():
             bid_amount = form.cleaned_data.get("amount")
-
-            # Check if the bid amount has cents
             if bid_amount % 1 != 0:
                 messages.warning(
                     request,
@@ -213,17 +215,14 @@ class AuctionDetailView(LoginRequiredMixin, View):
                     artwork_id=artwork_id,
                     auction_id=auction_id,
                 )
-
             bid = form.save(commit=False)
             bid.bidder = request.user.profile
             bid.auction = auction
             bid.bid_time = timezone.now()
             bid.save()
-
             bid_placed.send(
                 sender=self.__class__, bid=bid, user=request.user
             )
-
             messages.success(
                 request, "Your bid was submitted successfully!"
             )
