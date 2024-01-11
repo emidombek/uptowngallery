f99c4430a6cc9dc24b8dd964f2b14e201092de1f
emidombek <emilia.maria.dombek@gmail.com>
2024-01-11 20:36:45 +0000

    Remove comment and add new comment to class

--- uptowngallery/apps.py
+++ uptowngallery/apps.py
@@ -1,8 +1,10 @@
-# apps.py
-
 import sys
 from django.apps import AppConfig
 from django.utils import timezone
+
+"""
+Cofig class that schedules django q tasks
+"""
 
 
 class UptowngalleryConfig(AppConfig):
@@ -10,13 +12,9 @@ class UptowngalleryConfig(AppConfig):
 
     def ready(self):
         import uptowngallery.signals
-
-        # Import models when the app is ready
         from django_q.models import Schedule
 
         task_name = "uptowngallery.tasks.check_and_close_auctions"
-
-        # Check if the task is already scheduled
         if not Schedule.objects.filter(
             func=task_name, next_run__gt=timezone.now()
         ).exists():
@@ -26,5 +24,5 @@ class UptowngalleryConfig(AppConfig):
                 func=task_name,
                 schedule_type="I",
                 minutes=60,
-                name="Unique Name for My Task",  # Adding a unique name to the task
+                name="Unique Name for My Task",
             )
