5355ac9723b12440794bca4072a5c1492d862986
emidombek <emilia.maria.dombek@gmail.com>
2024-01-11 20:37:47 +0000

    Move code comment

--- uptowngallery/tasks.py
+++ uptowngallery/tasks.py
@@ -1,12 +1,12 @@
-"""
-Djano Q task that checks if the auction time has elasped and then changes the status of the auction to closed.
-"""
 from django.utils import timezone
 from .models import Auction
 from .signals import auction_closed
 
 
 def check_and_close_auctions():
+    """
+    Djano Q task that checks if the auction time has elasped and then changes the status of the auction to closed.
+    """
     now = timezone.now()
     active_auctions = Auction.objects.filter(
         end_date__lte=now, is_active=True
