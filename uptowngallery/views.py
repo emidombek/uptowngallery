5eca556d8e33f300ee929adc1749225a997d769f
emidombek <emilia.maria.dombek@gmail.com>
2024-03-01 14:08:05 +0000

    Update view to display error when seller attempts to bid on own auction

--- uptowngallery/views.py
+++ uptowngallery/views.py
@@ -238,34 +238,28 @@ class AuctionDetailView(LoginRequiredMixin, View):
         else:
             return render(request, "auction_detail.html", context)
 
-    def post(self, request, artwork_id, auction_id):
-        artwork = get_object_or_404(Artwork, pk=artwork_id)
-        auction = get_object_or_404(
-            Auction, pk=auction_id, artwork=artwork
-        )
-        form = BidForm(request.POST, auction=auction)
-        if form.is_valid():
-            bid = form.save(commit=False)
-            bid.bidder = request.user.profile
-            bid.auction = auction
-            bid.bid_time = timezone.now()
-            bid.save()
-            bid_placed.send(
-                sender=self.__class__, bid=bid, user=request.user
-            )
-            messages.success(
-                request, "Your bid was submitted successfully!"
-            )
-            return redirect(
-                "auction_detail",
-                artwork_id=artwork_id,
-                auction_id=auction_id,
-            )
-        else:
-            for error in form.errors.values():
-                messages.error(request, error)
-            return self.get(request, artwork_id, auction_id)
+def post(self, request, artwork_id, auction_id):
+    artwork = get_object_or_404(Artwork, pk=artwork_id)
+    auction = get_object_or_404(Auction, pk=auction_id, artwork=artwork)
+    # Check if the current user is the seller of the auction
+    if request.user.profile == auction.seller:
+        messages.error(request, "You cannot bid on your own auction.")
+        return self.get(request, artwork_id, auction_id)
 
+    form = BidForm(request.POST, auction=auction)
+    if form.is_valid():
+        bid = form.save(commit=False)
+        bid.bidder = request.user.profile
+        bid.auction = auction
+        bid.bid_time = timezone.now()
+        bid.save()
+        bid_placed.send(sender=self.__class__, bid=bid, user=request.user)
+        messages.success(request, "Your bid was submitted successfully!")
+        return redirect("auction_detail", artwork_id=artwork_id, auction_id=auction_id)
+    else:
+        for error in form.errors.values():
+            messages.error(request, error)
+        return self.get(request, artwork_id, auction_id)
 
 class ProfileInfoView(LoginRequiredMixin, View):
     """
