3a61fff828e592c9efaebbe16e8f76838221538c
emidombek <emilia.maria.dombek@gmail.com>
2024-04-28 21:38:59 +0100

    Add bootstrap paginitaion

--- uptowngallery/views.py
+++ uptowngallery/views.py
@@ -81,7 +81,7 @@ class ArtworkListView(View):
                 to_attr="recent_auction",
             )
         )
-        paginator = Paginator(artworks, 10)
+        paginator = Paginator(artworks, 12)
         page_number = request.GET.get("page")
         page_obj = paginator.get_page(page_number)
 
